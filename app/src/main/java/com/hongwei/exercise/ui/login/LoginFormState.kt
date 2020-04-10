95f22ffe3401db378561f229bec0c7fc9fcc06e7
Hongwei Bai <bhw8412@hotmail.com>
2020-04-10 14:17:46 +1000

    init repo

---
+++ app/src/main/java/com/hongwei/exercise/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.hongwei.exercise.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
